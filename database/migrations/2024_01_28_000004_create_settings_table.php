3d0dbd7a67a8599c2ee04a5d5c5ec4b8a206a36d
glr5048 <glr5048@gmail.com>
2024-01-29 21:34:32 -0500

    Updating migration classes

--- database/migrations/2024_01_28_000004_create_settings_table.php
+++ database/migrations/2024_01_28_000004_create_settings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAffiliateSettingsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -34,6 +34,6 @@ class CreateAffiliateSettingsTable extends Migration
     {
         Schema::dropIfExists('payments');
     }
-}
+};
 
 
