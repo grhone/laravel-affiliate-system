7428647c71e898849dff4790f477a68ff415ecb6
glr5048 <glr5048@gmail.com>
2024-01-29 21:42:05 -0500

    Fixing migration

--- database/migrations/2024_01_28_000004_create_affiliate_settings_table.php
+++ database/migrations/2024_01_28_000004_create_affiliate_settings_table.php
@@ -32,7 +32,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('payments');
+        Schema::dropIfExists('affiliate_settings');
     }
 };
 
