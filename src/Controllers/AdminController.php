d9f6176ebd1397139277f9b1e57e14a9db5c0e62
glr5048 <glr5048@gmail.com>
2024-02-01 16:22:17 -0500

    Updating admin panel

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -154,7 +154,7 @@ class AdminController extends Controller
         // Validate and update logic here
         $affiliate->update($request->all());
 
-        return redirect()->route('admin.affilate.show')
+        return redirect()->route('admin.affilate.show', ['id' => $id])
                         ->with('success', 'Affiliate updated successfully.');
     }
 
