b8341c71a51f3ec7f1eeaace17c3d880a6fe2233
glr5048 <glr5048@gmail.com>
2024-02-02 22:51:29 -0500

    Update AffiliateWelcomeMail.php

--- src/Mail/AffiliateWelcomeMail.php
+++ src/Mail/AffiliateWelcomeMail.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Grhone\LaravelAffiliateSystem\Models\Affiliate;
+use Illuminate\Mail\Mailables\Content;
 
 class AffiliateWelcomeMail extends Mailable
 {
@@ -18,12 +19,19 @@ class AffiliateWelcomeMail extends Mailable
         $this->affiliate = $affiliate;
     }
 
-    public function build()
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
     {
-        return $this->markdown('laravel-affiliate-system::emails.affiliate_welcome')
-                    ->subject( config('app.name') . ' - Welcome to Our Affiliate Program!')
-                    ->with([
+        return new Content(
+                    markdown:   'laravel-affiliate-system::emails.affiliate_welcome',
+                    subject:    config('app.name') . ' - Welcome to Our Affiliate Program!',
+                    with:
+                    [
                         'affiliate' => $this->affiliate,
-                    ]);
+                    ]
+                );
+
     }
 }
